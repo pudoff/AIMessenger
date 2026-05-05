8bad91b733936613c06cc1713134b6bc13172911
deqardis <leskov760@gmail.com>
2026-05-05 17:35:11 +0500

    feat: devops infra

---
+++ 4_Sources/backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+
+exec gunicorn config.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --log-level info
